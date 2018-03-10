9e3db83ab0a9ceb7a8cec7cd7af1774c399c095f
Jackson Turner <jjtparadox@gmail.com>
2018-03-10 12:26:30 -0600

    Updated license year

--- src/main/kotlin/com/jjtparadox/barometer/Barometer.kt
+++ src/main/kotlin/com/jjtparadox/barometer/Barometer.kt
@@ -1,7 +1,7 @@
 /*
  * This file is part of Barometer
  *
- * Copyright (c) 2017 jjtParadox
+ * Copyright (c) 2018 jjtParadox
  *
  * Barometer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
